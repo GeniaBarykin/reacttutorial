1e9d19eb760eab1db99b9bcda5c44f5a5438b7d3
GeniaBarykin <467232@student.saxion.nl>
2024-03-30 18:42:25 +0300

    3 lesson

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {User} from './User';
 
 function App() {
-    return (
-    <div className="App">
-      <SalaryComponent sal={10000} pos="DevOps"/>
-    </div>
-  );
-}
+  const age = 18;
+  const isGreen = true;
+  const names = [
+    { name: "Jenia", age: 21}, { name:"Sergey", age: 23}, { name:"Casha", age: 25},];
 
-const SalaryComponent = (p) =>{
-  return (
-    <div> 
-      <h1>{p.sal}</h1>
-      <h2>{p.pos}</h2>
-    </div>
-  );
+    return (    
+      <div className="App">
+      {names.map((value, key) => {
+        return <User key={key} name={value.name} age={value.age}></User> 
+      })}
+      {/* {age >= 18 ? <h1> Over age </h1> : <h1> Under age</h1>}
+      <h1 style={{color: isGreen ? "green" : "red"}}>This has color</h1>
+      {isGreen && <button>Button</button>} */}
+    </div>);
+  
 }
 
 export default App;
